b237668c0d8d82e4588f9026b470e742a9c2f595
Ilia Liubin <257798@student.pwr.edu.pl>
2021-04-26 00:11:13 +0300

    Wrzucenie plikow, ale aktualnie jeszcze nie zawieraja calosci, postaram sie jak najszybciej dopisac program

---
+++ src/rectangle.cpp
@@ -0,0 +1,78 @@
+
+#include "rectangle.hh"
+#include "../include/matrix.hh"
+#include <iostream>
+#include <cmath>
+#include "../include/size.hh"
+
+// Konstruktor bezparametryczny
+Rectangle::Rectangle()
+{
+    double args1[2] = {2,3}, args2[2] = {0,3}, args3[2] = {0,1}, args4[2] = {2,1};
+    v1 = Vector(args1);
+    v2 = Vector(args2);
+    v3 = Vector(args3);
+    v4 = Vector(args4);
+}
+
+/******************************************************************************
+ |  Konstruktor klasy Rectangle                                                   |
+ |  Argumenty:                                                                    |
+ |      v1X, v2X, v3X, v4X - cztery wektory, ktore reprezentuja polozenie wierzcholkow                                                      |
+ |  Zwraca:                                                                       |
+ |      Macierz wypelniona wartoscia 0.                                           |
+ */
+Rectangle::Rectangle(Vector const &v1X, Vector const &v2X, Vector const &v3X, Vector const &v4X)
+{
+    v1 = v1X;
+    v2 = v2X;
+    v3 = v3X;
+    v4 = v4X;
+    Rectangle a;
+    a = *this;
+    if (!a.check_length()){
+           std::cerr << "ERROR: przeciwlegle boki nie sa rowne!" << std::endl;
+    }
+}
+
+Rectangle Rectangle::translation(Vector const &t) const
+{
+    Rectangle translate;
+    translate.v1 = v1 + t;
+    translate.v2 = v2 + t;
+    translate.v3 = v3 + t;
+    translate.v4 = v4 + t;
+
+    return translate;
+}
+
+void Rectangle::Writetofile(std:: string filename, int mode)
+{
+        std::ofstream Dataf;
+        if(mode == 1)
+        Dataf.open(filename, std::ios::trunc);
+        else
+        Dataf.open(filename, std::ios::app);
+
+        fclose(stdout);
+}
+
+std::ostream& operator << (std::ostream &out, Rectangle const &Rec)
+{
+    out << Rec.v1 << std::endl;
+    out << Rec.v2 << std::endl;
+    out << Rec.v3 << std::endl;
+    out << Rec.v4 << std::endl;
+    return out;
+}
+
+std::ofstream& operator << (std::ofstream &fout, Rectangle const &Rec)
+{
+    fout << std::setprecision(10) << std::fixed;
+    fout << Rec.v1 << std::endl;
+    fout << Rec.v2 << std::endl;
+    fout << Rec.v3 << std::endl;
+    fout << Rec.v4 << std::endl;
+    return fout;
+}
+
---
+++ src/vector.cpp
@@ -0,0 +1,201 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include "vector.hh"
+#include "size.hh"
+
+
+/******************************************************************************
+ |  Konstruktor klasy Vector.                                                 |
+ |  Argumenty:                                                                |
+ |      Brak argumentow.                                                      |
+ |  Zwraca:                                                                   |
+ |      Tablice wypelniona wartoscia 0.                                       |
+ */
+Vector::Vector() {
+    for (int i = 0; i < SIZE; ++i) {
+        size[i] = 0;
+    }
+}
+
+
+/******************************************************************************
+ |  Konstruktor klasy Vector.                                                 |
+ |  Argumenty:                                                                |
+ |      tmp - Jednowymiarowa tablica typu double.                             |
+ |  Zwraca:                                                                   |
+ |      Tablice wypelniona wartosciami podanymi w argumencie.                 |
+ */
+
+Vector::Vector(double tmp[SIZE]) {
+    for (int i = 0; i < SIZE; ++i) {
+        size[i] = tmp[i];
+    }
+}
+
+
+/******************************************************************************
+ |  Realizuje dodawanie dwoch wektorow.                                       |
+ |  Argumenty:                                                                |
+ |      this - pierwszy skladnik dodawania,                                   |
+ |      v - drugi skladnik dodawania.                                         |
+ |  Zwraca:                                                                   |
+ |      Sume dwoch skladnikow przekazanych jako wskaznik                      |
+ |      na parametr.                                                          |
+ */
+Vector Vector::operator + (const Vector &v) {
+    Vector result;
+    for (int i = 0; i < SIZE; ++i) {
+        result[i] = size[i] += v[i];
+    }
+    return result;
+}
+
+
+/******************************************************************************
+ |  Realizuje odejmowanie dwoch wektorow.                                     |
+ |  Argumenty:                                                                |
+ |      this - pierwszy skladnik odejmowania,                                 |
+ |      v - drugi skladnik odejmowania.                                       |
+ |  Zwraca:                                                                   |
+ |      Roznice dwoch skladnikow przekazanych jako wskaznik                   |
+ |      na parametr.                                                          |
+ */
+Vector Vector::operator - (const Vector &v) {
+    Vector result;
+    for (int i = 0; i < SIZE; ++i) {
+        result[i] = size[i] -= v[i];
+    }
+    return result;
+}
+
+
+/******************************************************************************
+ |  Realizuje mnozenie wektora przez liczbe zmiennoprzecinkowa.               |
+ |  Argumenty:                                                                |
+ |      this - pierwszy skladnik mnozenia (wektor),                           |
+ |      v - drugi skladnik mnozenia (liczba typu double).                     |
+ |  Zwraca:                                                                   |
+ |      Iloczyn dwoch skladnikow przekazanych jako wskaznik                   |
+ |      na parametr.                                                          |
+ */
+
+Vector Vector::operator * (const double &tmp) {
+    Vector result;
+    for (int i = 0; i < SIZE; ++i) {
+        result[i] = size[i] *= tmp;
+    }
+    return result;
+}
+
+
+/******************************************************************************
+ |  Realizuje dzielenie dwoch wektorow.                                       |
+ |  Argumenty:                                                                |
+ |      this - licznik dzielenia,                                             |
+ |      v - mianownik dzielenia.                                              |
+ |  Zwraca:                                                                   |
+ |      Iloraz dwoch skladnikow przekazanych jako wskaznik                    |
+ |      na parametr.                                                          |
+ */
+
+Vector Vector::operator / (const double &tmp) {
+    Vector result;
+
+    for (int i = 0; i < SIZE; ++i) {
+        result[i] = size[i] / tmp;
+    }
+
+    return result;
+}
+
+
+/******************************************************************************
+ |  Funktor wektora.                                                          |
+ |  Argumenty:                                                                |
+ |      index - index wektora.                                                |
+ |  Zwraca:                                                                   |
+ |      Wartosc wektora w danym miejscu tablicy jako stala.                   |
+ */
+const double &Vector::operator [] (int index) const {
+    if (index < 0 || index >= SIZE) {
+        std::cerr << "Error: Wektor jest poza zasiegiem!" << std::endl;
+    }
+    return size[index];
+}
+
+
+/******************************************************************************
+ |  Funktor wektora.                                                          |
+ |  Argumenty:                                                                |
+ |      index - index wektora.                                                |
+ |  Zwraca:                                                                   |
+ |      Wartosc wektora w danym miejscu tablicy.                              |
+ */
+double &Vector::operator[](int index) {
+    return const_cast<double &>(const_cast<const Vector *>(this)->operator[](index));
+}
+
+
+/******************************************************************************
+ |  Przeciazenie operatora <<                                                 |
+ |  Argumenty:                                                                |
+ |      out - strumien wejsciowy,                                             |
+ |      tmp - wektor.                                                         |
+ */
+std::ostream &operator << (std::ostream &out, Vector const &tmp) {
+    for (int i = 0; i < SIZE; ++i) {
+        out << "[ " << tmp[i] << " ]\n";
+    }
+    return out;
+}
+
+
+/******************************************************************************
+ |  Przeciazenie operatora >>                                                 |
+ |  Argumenty:                                                                |
+ |      in - strumien wyjsciowy,                                              |
+ |      tmp - wektor.                                                         |
+ */
+std::istream &operator >> (std::istream &in, Vector &tmp) {
+    for (int i = 0; i < SIZE; ++i) {
+        in >> tmp[i];
+    }
+    std::cout << std::endl;
+    return in;
+}
+
+
+/******************************************************************************
+ |  Metoda rotacji wektora                                               |
+ |  Argumenty:                                                                |
+ |      angle - kat obrotu (podany w stopniach),                                              |
+ |  Zwraca:
+        obracany wektor                                                       |
+ */
+Vector Vector::rotation(const double &angle) const
+{
+    Vector rotate;
+    double angle_radian = angle * M_PI / 180;
+    if(SIZE == 2)
+    {
+        double tmp[][SIZE] = {{cos(angle_radian), -sin(angle_radian)}, {sin(angle_radian), cos(angle_radian)}};
+        Matrix transformation(tmp);
+        rotate = transformation * *this;
+    }
+    else
+    {
+      std::cerr << "ERROR: Nie zdefiniowano macierzy obrotu dla przestrzeni innej niz dwuwymiarowa." << std::endl;
+    }
+    return rotate;
+}
+
+bool Vector::operator == (const Vector v) const
+{
+    if(abs(size[0] - v[0]) < MIN_DIFF && abs(size[1]- v[1]) < MIN_DIFF)
+    {
+        return true;
+    }
+    return false;
+}
