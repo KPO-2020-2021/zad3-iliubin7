1695640e3b1fae7529b527bb4485ffe011912d96
Ilia Liubin <257798@student.pwr.edu.pl>
2021-04-30 18:09:09 +0300

    Dodanie metody sprawdzania dlugosci bokow prostokata

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -75,7 +75,10 @@ void Rectangle::Writetofile(std:: string filename, int mode)
 
 void Rectangle::new_rectangle(Vector &v1X, Vector &v2X, Vector &v3X, Vector &v4X) const
 {
-     
+     v1 = v1X;
+     v2 = v2X;
+     v3 = v3X;
+     v4 = v4X;
 }
 
 /******************************************************************************
@@ -105,7 +108,49 @@ bool Rectangle::rotate(const double angle, int n) const
 // metoda sprawdzania dlugosci bokow prostokata
 bool Rectangle::check_length() const
 {
+   double length[4];
+   length[0] = v2[0] - v1[0];
+   length[1] = v3[0] - v4[0];
    
+   length[2] = v4[1] - v1[1];
+   length[3] = v3[1] - v2[1];
+   if(length[0] < length[1])
+   {
+       // Boki length[0] i length[1] sa dluzsze, a boki length[2] i length[3] sa krotsze
+     if(length[0] == length[1])
+      std::cout << "Dluzsze przeciwlegle boki sa sobie rowne." << std::endl;
+      else
+      std::cout << "Dluzsze przeciwlegle boki nie sa sobie rowne!!!" << std::endl;
+    // wyswietlanie dlugosci dluzszych bokow
+    std::cout << "Dlugosc pierwszego boku:" << std::fixed << std::setprecision(20) << length[0] << std::endl;
+    std::cout << "Dlugosc drugiego boku:" << std::fixed << std::setprecision(20) << length[1] << std::endl;
+    if(length[2] == length[3])
+    std::cout << "Krotsze przeciwlegle boki sa sobie rowne." << std::endl;
+    else
+    std::cout << "Krotsze przeciwlegle boki nie sa sobie rowne!!!" << std::endl;
+    // wyswietlanie dlugosci krotszych bokow
+    std::cout << "Dlugosc pierwszego boku:" << std::fixed << std::setprecision(20) << length[2] << std::endl;
+    std::cout << "Dlugosc drugiego boku:" << std::fixed << std::setprecision(20) << length[3] << std::endl;
+    }
+    else
+    {
+        // Boki length[0] i length[1] sa krotsze, a boki length[2] i length[3] sa dluzsze
+        if(length[2] == length[3])
+        std::cout << "Dluzsze przeciwlegle boki sa sobie rowne." << std::endl;
+        else
+        std::cout << "Dluzsze przeciwlegle boki nie sa sobie rowne!!!" << std::endl;
+        // wyswietlanie dlugosci bokow
+        std::cout << "Dlugosc pierwszego boku:" << std::fixed << std::setprecision(20) << length[2] << std::endl;
+        std::cout << "Dlugosc drugiego boku:" << std::fixed << std::setprecision(20) << length[3] << std::endl;
+        if(length[0] == length[1])
+        std::cout << "Krotsze przeciwlegle boki sa sobie rowne." << std::endl;
+        else
+        std::cout << "Krotsze przeciwlegle boki nie sa sobie rowne!!!" << std::endl;
+        // wyswietlanie dlugosci krotszych bokow
+        std::cout << "Dlugosc pierwszego boku:" << std::fixed << std::setprecision(20) << length[0] << std::endl;
+        std::cout << "Dlugosc drugiego boku:" << std::fixed << std::setprecision(20) << length[1] << std::endl;
+    }
+    
 }
 
 /******************************************************************************
